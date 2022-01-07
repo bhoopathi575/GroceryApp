012bb7f2a6a760e639def3c855d01c77bd508678
shamlisampla <shamlisampla@gmail.com>
2022-01-07 18:38:01 -0500

    Add files via upload

--- constants/colors.js
+++ constants/colors.js
@@ -2,5 +2,7 @@ export default {
     primary: '#5956E9', //blue
     secondary: '#fff', // white
     offWhite: '#FAF9F6', // off white
-    lightGrey: '#D3D3D3' // light grey
+    lightGrey: '#D3D3D3', // light grey
+    orange: '#eb6716',
+    white: '#FFFFFF'
 }
