415bedd01868da51f8738cab0a3af309d5b019fd
shamlisampla <shamlisampla@gmail.com>
2022-01-26 07:48:09 -0500

    Add files via upload

--- constants/colors.js
+++ constants/colors.js
@@ -3,6 +3,8 @@ export default {
     secondary: '#fff', // white
     offWhite: '#FAF9F6', // off white
     lightGrey: '#D3D3D3', // light grey
-    orange: '#eb6716',
-    white: '#FFFFFF'
+    orange: '#eb6716',   //orange
+    white: '#FFFFFF',   //white
+    green: '#097969',  //green
+    red: '#DC143C', //red
 }
