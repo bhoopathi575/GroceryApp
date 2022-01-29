961263a5d5e60dfd933b4febefb2353a0b0305a8
sukhwindersingh7 <jasss73904@gmail.com>
2022-01-29 16:21:48 -0500

    Update colors.js

--- constants/colors.js
+++ constants/colors.js
@@ -2,5 +2,9 @@ export default {
     primary: '#5956E9', //blue
     secondary: '#fff', // white
     offWhite: '#FAF9F6', // off white
-    lightGrey: '#D3D3D3' // light grey
+    lightGrey: '#D3D3D3', // light grey
+    orange:    '#eb6716', //orange
+    white: '#FFFFFF',
+    green: '#097969',
+    red:'#DC143C',
 }
