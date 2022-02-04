6ceae5bfefb2e98aa54f2f50a1befdebcf6a6c97
shamlisampla <shamlisampla@gmail.com>
2022-02-03 23:01:26 -0500

    Add files via upload

--- models/item.js
+++ models/item.js
@@ -1,11 +1,12 @@
 class Item {
-    constructor(id, name, detail, price, uri, ratings) {
+    constructor(id, name, detail, price, uri, ratings, unit=null) {
         this.id = id; // unique & number
         this.name = name; // string
         this.detail = detail; //string
         this.price = price; // float
         this.uri = uri; // string
         this.ratings = ratings; //list of objects where object is of Rating Class.
+        this.unit = unit;
     }
 }
 
