5c2ac293b4cdb56c214bb58ef1ad6e635929bb77
sukhwindersingh7 <jasss73904@gmail.com>
2022-02-03 22:51:30 -0500

    Add files via upload

--- constants/colors.js
+++ constants/colors.js
@@ -3,8 +3,8 @@ export default {
     secondary: '#fff', // white
     offWhite: '#FAF9F6', // off white
     lightGrey: '#D3D3D3', // light grey
-    orange:    '#eb6716', //orange
-    white: '#FFFFFF',
-    green: '#097969',
-    red:'#DC143C',
+    orange: '#eb6716',   //orange
+    white: '#FFFFFF',   //white
+    green: '#097969',  //green
+    red: '#DC143C', //red
 }
